dbac92ac4d4d50bd3c12dafd29520fd78b3e4eed
AliReza7222 <ali.r.salehi99@gmail.com>
2023-08-01 13:06:15 +0430

    view custom model user created

---
+++ accounts/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+
+from .models import MyUser
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = MyUser
+        fields = ['username', 'email', 'password', 'confirm_password']
+
+
+    def validate_password(self, value):
+        if len(value) <= 6:
+            raise serializers.ValidationError("Password must be 6 characters longer !")
+        return value
+
+    def validate(self, data):
+        password, confirm = data['password'], data['confirm_password']
+        if password != confirm:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        del validated_data['confirm_password']
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
